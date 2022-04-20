0937905313cf7b0d76f0f5d0067fd1397516d8ba
Théo André <theoandre1907@gmail.com>
2022-04-20 02:07:16 +0200

    Update heat_equation.py

--- heat_equation.py
+++ heat_equation.py
@@ -7,8 +7,8 @@ from scipy.sparse.linalg import spsolve
 
 
 """ SETTING EVERYTHING UP (parameters for the model) """
-N = 1000 # inner points for the space mesh 
-T = 0.5 # final time
+N = 100 # inner points for the space mesh 
+T = 1 # final time
 
 
 a = 1 # diffusion coefficient
@@ -58,12 +58,16 @@ matrix_ch = sp.spdiags(
 
 u_list = [u_0]
 
+energy = [(np.sqrt(dx) * np.linalg.norm(u_0, 2))**2]
+
 # compute solution
 for j in range(int(T/dt)):
-    # if (j % 10 == 0) : print("check", j,"out of ", T/dt)
+    if (j % 1000 == 0) : print("check", j,"out of ", T/dt)
     u_next = spsolve(matrix_bh, matrix_ch @ u_list[j][1:-1])
     u_next = np.concatenate(([0], u_next, [0]))
     u_list.append(u_next)
+    
+    energy.append((np.sqrt(dx) * np.linalg.norm(u_next, 2))**2)
 
 """ --- end of computing solutions --- """
 
@@ -79,16 +83,6 @@ u_sol_2 = u_exact(0.5, x_mesh, k)
 """ --- end of true solution --- """
 
 
-
-""" COMPUTE ERRORS """
-
-e_h_1 = u_sol_1 - u_list[int(0.1/dt) + 1]
-print(e_h_1)
-
-""" --- end of computing errors"""
-
-
-
 """ PLOTTING STUFF """
 plt.style.use('ggplot')
 plt.plot(x_mesh, u_list[int(0.1/dt) + 1], c='blue',ls='--', lw=2, label="approx T=0.1")
@@ -99,4 +93,13 @@ plt.title(r"Approximation de la solution exacte par le $\theta$-schema")
 plt.legend()
 plt.show()
 
+
+
+plt.plot(np.linspace(0, 1, int(T/dt)), np.array([0.5 * exp(- 2 * k**2 * pi **2 * n * dt) for n in range(int(T/dt))]), c='red', ls='-', lw=0.8, label='Exact.')
+plt.plot(np.linspace(0, 1, int(T/dt) + 1), energy, marker='o', markevery=10000, c='blue', ls='--', dashes=(3, 3), lw = 2, label="Approx.")
+plt.xlabel(r'$t (temps)$')
+plt.ylabel(r'$\mathcal{E}^n$ vs. $E(t)$ (energie)')
+plt.legend()
+plt.title(r"Énergie de la solution $\mathbf{u}_h$ en fonction du temps")
+plt.show()
 """ --- end of plotting stuff --- """
