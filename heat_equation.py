96247758e8a8f8726127963dac14fd09894223f9
Théo André <46953688+theoanalyse@users.noreply.github.com>
2022-04-20 21:11:33 +0200

    new file case2

--- heat_equation.py
+++ heat_equation.py
@@ -5,14 +5,13 @@ import scipy.sparse as sp
 from scipy.sparse.linalg import spsolve
 
 
-
 """ SETTING EVERYTHING UP (parameters for the model) """
-N = 100 # inner points for the space mesh 
-T = 1 # final time
+N = 100  # inner points for the space mesh
+T = 1  # final time
 
 
-a = 1 # diffusion coefficient
-dx = 1 / (N + 1) # space step
+a = 1  # diffusion coefficient
+dx = 1 / (N + 1)  # space step
 dt = 1e-5
 nu = a * dt / (dx*dx)
 theta = 0.5
@@ -21,40 +20,43 @@ print("nu =", nu)
 
 M = int(1 / dt)
 
-x_mesh = np.linspace(0, 1, N+2) # mesh in space
+x_mesh = np.linspace(0, 1, N+2)  # mesh in space
 x_inner_mesh = x_mesh[1:N+1]
-t_mesh = np.linspace(0, T, M) # mesh in time
+t_mesh = np.linspace(0, T, M)  # mesh in time
 
-init_cond = lambda x, k: sin(k*pi*x) # initial condition of the Cauchy Problem 
+
+# initial condition of the Cauchy Problem
+def init_cond(x, k): return sin(k*pi*x)
+
 
 """ --- end of setup --- """
 
 """ IMPLEMENTATION OF THETA METHOD """
-k = 1 # parametrize k in initial condition
-u_0 = init_cond(x_inner_mesh, k) # initial conditions
-u_0 = np.concatenate(([0],u_0, [0])) # init cond. + border conditions
+k = 1  # parametrize k in initial condition
+u_0 = init_cond(x_inner_mesh, k)  # initial conditions
+u_0 = np.concatenate(([0], u_0, [0]))  # init cond. + border conditions
 
 # Create Bh := Tridiag(-theta * nu, 1 + 2 * theta * nu, -theta * nu)_n
 matrix_bh = sp.spdiags(
     [[-theta * nu for _ in range(N)],
-    [1 + 2 * theta * nu for _ in range(N)],
-    [- theta * nu for _ in range(N)]],
+     [1 + 2 * theta * nu for _ in range(N)],
+     [- theta * nu for _ in range(N)]],
     (-1, 0, 1),
     N,
     N,
     format='csc'
-    )
+)
 
 # create Ch := Tridiag((1-theta) * nu , 1 - 2 * (1 - theta) * nu, (1-theta) * nu)_n
 matrix_ch = sp.spdiags(
     [[(1 - theta) * nu for _ in range(N)],
-    [1 - 2 * (1 - theta) * nu for _ in range(N)],
-    [(1 - theta) * nu for _ in range(N)]],
+     [1 - 2 * (1 - theta) * nu for _ in range(N)],
+     [(1 - theta) * nu for _ in range(N)]],
     (-1, 0, 1),
     N,
     N,
     format='csc'
-    ) 
+)
 
 u_list = [u_0]
 
@@ -62,20 +64,22 @@ energy = [(np.sqrt(dx) * np.linalg.norm(u_0, 2))**2]
 
 # compute solution
 for j in range(int(T/dt)):
-    if (j % 1000 == 0) : print("check", j,"out of ", T/dt)
+    if (j % 1000 == 0):
+        print("check", j, "out of ", T/dt)
     u_next = spsolve(matrix_bh, matrix_ch @ u_list[j][1:-1])
     u_next = np.concatenate(([0], u_next, [0]))
     u_list.append(u_next)
-    
+
     energy.append((np.sqrt(dx) * np.linalg.norm(u_next, 2))**2)
 
 """ --- end of computing solutions --- """
 
 
-
 """ TRUE SOLUTION """
 
-u_exact = lambda t, x, k: exp(-k*k*pi*pi*t) * sin(k*pi*x)
+
+def u_exact(t, x, k): return exp(-k*k*pi*pi*t) * sin(k*pi*x)
+
 
 u_sol_1 = u_exact(0.1, x_mesh, k)
 u_sol_2 = u_exact(0.5, x_mesh, k)
@@ -85,7 +89,8 @@ u_sol_2 = u_exact(0.5, x_mesh, k)
 
 """ PLOTTING STUFF """
 plt.style.use('ggplot')
-plt.plot(x_mesh, u_list[int(0.1/dt) + 1], c='blue',ls='--', lw=2, label="approx T=0.1")
+plt.plot(x_mesh, u_list[int(0.1/dt) + 1], c='blue',
+         ls='--', lw=2, label="approx T=0.1")
 plt.plot(x_mesh, u_sol_1, c='blue', lw=0.8, label="exact T=0.1")
 plt.plot(x_mesh, u_list[-1], ls='--', c='red', lw=2, label="approx T=0.5")
 plt.plot(x_mesh, u_sol_2, c='red', lw=0.8, label="exact T=0.5")
@@ -94,9 +99,10 @@ plt.legend()
 plt.show()
 
 
-
-plt.plot(np.linspace(0, 1, int(T/dt)), np.array([0.5 * exp(- 2 * k**2 * pi **2 * n * dt) for n in range(int(T/dt))]), c='red', ls='-', lw=0.8, label='Exact.')
-plt.plot(np.linspace(0, 1, int(T/dt) + 1), energy, marker='o', markevery=10000, c='blue', ls='--', dashes=(3, 3), lw = 2, label="Approx.")
+plt.plot(np.linspace(0, 1, int(T/dt)), np.array([0.5 * exp(- 2 * k**2 * pi ** 2 * n * dt)
+         for n in range(int(T/dt))]), c='red', ls='-', lw=0.8, label='Exact.')
+plt.plot(np.linspace(0, 1, int(T/dt) + 1), energy, marker='o',
+         markevery=10000, c='blue', ls='--', dashes=(3, 3), lw=2, label="Approx.")
 plt.xlabel(r'$t (temps)$')
 plt.ylabel(r'$\mathcal{E}^n$ vs. $E(t)$ (energie)')
 plt.legend()
--- heat_errors.py
+++ heat_errors.py
@@ -6,17 +6,17 @@ from scipy.sparse.linalg import spsolve
 plt.style.use('ggplot')
 
 
+""" SETTING EVERYTHING UP (parameters for the model) """
+T = 0.5  # final time
 
-""" SETTING EVERYTHING UP (parameters for the model) """ 
-T = 0.5 # final time
 
-
-a = 1 # diffusion coefficient
+a = 1  # diffusion coefficient
 nu = 0.5
-Ns = np.array([int(1e1), int(1e2), int(1e3)]) # inner points for the space mesh
+# inner points for the space mesh
+Ns = np.array([int(1e1), int(1e2), int(1e3)])
 errors_list = []
 
-thetas = [0, 1, 0.5] # method chosen
+thetas = [0, 1, 0.5]  # method chosen
 
 for theta in thetas:
     errors = []
@@ -24,65 +24,63 @@ for theta in thetas:
     dxs = []
     dts = []
     for N in Ns:
-        dx = 1 / (N + 1) # space step
+        dx = 1 / (N + 1)  # space step
         dt = nu * dx * dx / a
-
 
         dxs.append(dx)
         dts.append(dt)
 
         M = int(1 / dt)
 
-        x_mesh = np.linspace(0, 1, N+2) # mesh in space
+        x_mesh = np.linspace(0, 1, N+2)  # mesh in space
         x_inner_mesh = x_mesh[1:N+1]
-        t_mesh = np.linspace(0, T, M) # mesh in time
+        t_mesh = np.linspace(0, T, M)  # mesh in time
 
-        init_cond = lambda x, k: sin(k*pi*x) # initial condition of the Cauchy Problem 
-
+        # initial condition of the Cauchy Problem
+        def init_cond(x, k): return sin(k*pi*x)
         """ --- end of setup --- """
 
-            
         """ TRUE SOLUTION """
 
-        k = 1 # parametrize k in initial condition
-        u_exact = lambda t, x, k: exp(-k*k*pi*pi*t) * sin(k*pi*x)
+        k = 1  # parametrize k in initial condition
+        def u_exact(t, x, k): return exp(-k*k*pi*pi*t) * sin(k*pi*x)
         u_sol = u_exact(0.5, x_mesh, k)
 
         """ --- end of true solution --- """
 
-
         """ IMPLEMENTATION OF THETA METHOD """
-        u_0 = init_cond(x_inner_mesh, k) # initial conditions
-        u_0 = np.concatenate(([0], u_0, [0])) # init cond. + border conditions
+        u_0 = init_cond(x_inner_mesh, k)  # initial conditions
+        u_0 = np.concatenate(([0], u_0, [0]))  # init cond. + border conditions
 
         # Create Bh := Tridiag(-theta * nu, 1 + 2 * theta * nu, -theta * nu)_n
         matrix_bh = sp.spdiags(
             [[-theta * nu for _ in range(N)],
-            [1 + 2 * theta * nu for _ in range(N)],
-            [- theta * nu for _ in range(N)]],
+             [1 + 2 * theta * nu for _ in range(N)],
+             [- theta * nu for _ in range(N)]],
             (-1, 0, 1),
             N,
             N,
             format='csc'
-            )
+        )
 
         # create Ch := Tridiag((1-theta) * nu , 1 - 2 * (1 - theta) * nu, (1-theta) * nu)_n
         matrix_ch = sp.spdiags(
             [[(1 - theta) * nu for _ in range(N)],
-            [1 - 2 * (1 - theta) * nu for _ in range(N)],
-            [(1 - theta) * nu for _ in range(N)]],
+             [1 - 2 * (1 - theta) * nu for _ in range(N)],
+             [(1 - theta) * nu for _ in range(N)]],
             (-1, 0, 1),
             N,
             N,
             format='csc'
-            ) 
+        )
 
         u_list = [u_0]
 
-        # compute solution with 
+        # compute solution with
         print(int(T/dt))
         for j in range(int(T/dt)):
-            if (j % 100 == 0) : print("check", j,"out of ", T/dt)
+            if (j % 100 == 0):
+                print("check", j, "out of ", T/dt)
             u_next = spsolve(matrix_bh, matrix_ch @ u_list[j][1:-1])
             u_next = np.concatenate(([0], u_next, [0]))
             u_list.append(u_next)
@@ -90,7 +88,7 @@ for theta in thetas:
         """ --- end of computing solutions --- """
 
         e_h = u_sol - u_list[-1]
-        errors.append(np.max(np.abs(e_h))) # error L^infty
+        errors.append(np.max(np.abs(e_h)))  # error L^infty
         # errors.append(np.sqrt(dx) * np.linalg.norm(e_h, 2)) # errors L^2 discrete
 
         '''
@@ -99,9 +97,8 @@ for theta in thetas:
         plt.legend()
         plt.show()
         '''
-    
-    errors_list.append(errors)
 
+    errors_list.append(errors)
 
 
 dxs = np.array(dxs)
